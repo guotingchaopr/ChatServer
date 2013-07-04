dbb2be439c64d4889d6fc1cf4f001e0f0e13b444
guotingchaopr <guotingchaopr@gmail.com>
2013-07-04 23:36:28 +0800

    Init commit

---
+++ src/com/guotingchao/TaskChat/Server/TaskServerSocket.java
@@ -0,0 +1,114 @@
+package com.guotingchao.TaskChat.Server;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.websocket.WebSocket.OnTextMessage;
+
+import com.guotingchao.TaskChat.Servlet.InitServerServlet;
+
+/**
+ * 
+ * @ClassName: TaskServerSocket
+ * @Description: TODO(通讯主程)
+ * @author A18ccms a18ccms_gmail_com
+ * @date 2013-7-4 下午7:58:34
+ * 
+ */
+public class TaskServerSocket implements OnTextMessage {
+	private Logger log = Logger.getLogger(TaskServerSocket.class);
+	private Connection conn;
+	private Long userid;
+
+	/**
+	 * <p>
+	 * Title:
+	 * </p>
+	 * <p>
+	 * Description:
+	 * </p>
+	 */
+	public TaskServerSocket(Long userId) {
+		this.userid = userId;
+	}
+
+	/**
+	 * [重载方法]
+	 * <p>
+	 * Title: onClose
+	 * </p>
+	 * <p>
+	 * Description:当某个连接断开时 触发此方法
+	 * </p>
+	 * 
+	 * @param index
+	 * @param name
+	 * @see org.eclipse.jetty.websocket.WebSocket#onClose(int, java.lang.String)
+	 */
+	@Override
+	public void onClose(int index, String time) {
+		InitServerServlet.getTaskServerSocketList().remove(this);
+		log.info(this.userid+" 断开：" + time);
+	}
+
+	/**
+	 * [重载方法]
+	 * <p>
+	 * Title: onOpen
+	 * </p>
+	 * <p>
+	 * Description: 当某个连接联通时
+	 * </p>
+	 * 
+	 * @param conn
+	 * @see org.eclipse.jetty.websocket.WebSocket#onOpen(org.eclipse.jetty.websocket.WebSocket.Connection)
+	 */
+	@Override
+	public void onOpen(Connection conn) {
+		log.info("新用户" +this.userid +"接入");
+		this.conn = conn;
+		InitServerServlet.getTaskServerSocketList().add(this);
+	}
+
+	/**
+	 * [重载方法]
+	 * <p>
+	 * Title: onMessage
+	 * </p>
+	 * <p>
+	 * Description: 当接收到消息时触发
+	 * </p>
+	 * 
+	 * @param message
+	 * @see org.eclipse.jetty.websocket.WebSocket.OnTextMessage#onMessage(java.lang.String)
+	 */
+	@Override
+	public void onMessage(String message) {
+		List<TaskServerSocket> socketList = InitServerServlet
+				.getTaskServerSocketList();
+		for (TaskServerSocket socket : socketList) {
+			try {
+				socket.getConn().sendMessage(this.userid+" 说:"+message);
+			} catch (IOException e) {
+				log.error(e.getMessage());
+			}
+		}
+	}
+
+	public Connection getConn() {
+		return conn;
+	}
+
+	public void setConn(Connection conn) {
+		this.conn = conn;
+	}
+
+	public Long getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+}
---
+++ src/com/guotingchao/TaskChat/Servlet/TaskMainServlet.java
@@ -0,0 +1,44 @@
+/**   
+* @Title: TaskMainServlet.java
+* @Package com.guotingchao.TaskChat.Servlet
+* @Description: TODO(用一句话描述该文件做什么)
+* @author A18ccms A18ccms_gmail_com   
+* @date 2013-7-4 下午8:22:18
+* @version V1.0   
+*/
+package com.guotingchao.TaskChat.Servlet;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.websocket.WebSocket;
+import org.eclipse.jetty.websocket.WebSocketServlet;
+
+import com.guotingchao.TaskChat.Server.TaskServerSocket;
+
+/**
+ * @ClassName: TaskMainServlet
+ * @Description: TODO(TaskSocket分发servlet)
+ * @author A18ccms a18ccms_gmail_com
+ * @date 2013-7-4 下午8:22:18
+ * 
+ */
+public class TaskMainServlet extends WebSocketServlet{
+	private static final long serialVersionUID = 1L;
+	private Logger log = Logger.getLogger(TaskMainServlet.class);
+	/**
+	 * [重载方法]
+	 * <p>Title: doWebSocketConnect</p>
+	 * <p>Description: </p>
+	 * @param arg0
+	 * @param arg1
+	 * @return
+	 * @see org.eclipse.jetty.websocket.WebSocketFactory.Acceptor#doWebSocketConnect(javax.servlet.http.HttpServletRequest, java.lang.String)
+	 */
+	@Override
+	public WebSocket doWebSocketConnect(HttpServletRequest request, String args) {
+		Long userId= Long.parseLong(request.getParameter("userid"));
+		log.info("args: "+args +" \t userId: " +userId);
+		return new TaskServerSocket(userId);
+	}
+}
